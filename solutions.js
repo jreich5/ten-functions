cbcbdf63340665b22af8dde8c375d10094a135c6
Justin Reich <justinreich.dev@gmail.com>
2022-11-29 14:11:32 -0600

    feat: Add solutions

--- solutions.js
+++ solutions.js
@@ -21,3 +21,154 @@
  * concat
  */
 
+function isTrue(input) {
+    let output;
+    if (input === true) {
+        output = true;
+    } else {
+        output = false;
+    }
+    return output;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+// 3. Define a function named `not` that takes in any input and returns the boolean opposite of the provided input.
+//
+// not(false)                  // true
+// not(0)                      // true
+// not("")                     // true
+// not(null)                   // true
+// not(NaN)                    // true
+// not(undefined)              // true
+// not(true)                   // false
+// not("something")            // false
+// not(Infinity)               // false
+// not(123)                    // false
+
+// falsey values: empty strings, NaN, null, undefined, 0
+
+// not('false')                // false
+
+function not(input) {
+    return !input;
+}
+
+
+// 4. Define a function named `addOne` that takes in a single input. If the input is a number or a numeric string, return the value plus one.
+//
+// addOne(0)                    // 1
+// addOne(2)                    // 3
+// addOne(-5)                   // -4
+// addOne(5.789)                // 6.789
+// addOne(Infinity)             // Infinity
+// addOne("2")                  // 3
+// addOne("0")                  // 1
+// addOne("banana")             // NaN
+// addOne(true)                 // NaN
+// addOne(NaN)                  // NaN
+
+function addOne(input) {
+    let number = parseFloat(input);
+    return number + 1;
+}
+
+// function addOne(input) {
+//     input = input + 1;
+//     return ++input;
+// }
+
+
+
+// 5. Define a function named `isEven` that takes in a single input. If the input is an even number or a string containing an even number, return `true`. Any other input should return false for the output.
+//
+// isEven(2)                   // true
+// isEven(-8)                  // true
+// isEven(0)                   // true
+// isEven("42")                // true
+// isEven(1)                   // false
+// isEven("-3")                // false
+// isEven(false)               // false
+// isEven("banana")            // false
+
+function isEven(input) {
+    if (typeof input == 'boolean') {
+        return false;
+    }
+    return input % 2 === 0;
+}
+
+
+
+
+
+
+// 6. Define a function named `isIdentical` that takes in two input arguments. If each input is equal both in data type and in value, then return `true`. If the values are not the same data type or not the same value, return `false`.
+//
+// isIdentical(3, 3)                     // true
+// isIdentical(false, false)             // true
+// isIdentical("hello", "hello")         // true
+// isIdentical(3, 3.0)                   // true
+// isIdentical(undefined, undefined)     // true
+// isIdentical(2, "2")                   // false
+// isIdentical("javascript", "java")     // false
+
+function isIdentical(input1, input2) {
+    return input1 === input2;
+}
+
+// 7. Define a function named `isEqual` that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+//
+// isEqual(3, "3")                       // true
+// isEqual("abc123", "abc123")           // true
+// isEqual(true, 1)                      // true
+// isEqual(0, false)                     // true
+// isEqual(4, -5)                        // false
+// isEqual("java", "javascript")         // false
+
+function isEqual(input1, input2) {
+    return input1 == input2;
+}
+
+
+// 8. Define a function named `or` that takes in two input arguments. The output returned should be the result of an `or` operation on both inputs.
+//
+// or(true, true)                    // true
+// or(true, false)                   // true
+// or(false, true)                   // true
+// or(false, false)                  // false
+// or("hello", "world")              // "hello" (this behavior is non-obvious, research more!)
+
+function or(input1, input2) {
+    return input1 || input2;
+}
+//
+//
+// 9. Define a function named `and` that takes in two input arguments and returns the result of a logical `and` operation of both inputs.
+//
+// and(true, true)                    // true
+// and(true, false)                   // false
+// and(false, true)                   // false
+// and(false, false)                  // false
+// and("hello", "world")              // "world" (this behavior is non-obvious, research more)
+
+function and(input1, input2) {
+    return input1 && input2;
+}
+
+
+
+// 10. Define a function named `concat` that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals.
+//
+// concat("code", "up")                // "codeup"
+// concat("connect", 4)                // "connect4"
+// concat("hello", "world")            // "helloworld"
+// concat(4, 2)                        // "42"
+// concat(true, true)                  // "truetrue"
+
+function concat(input1, input2) {
+    return String(input1) + String(input2);
+}
+
